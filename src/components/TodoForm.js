cd8b56e144008399ea6829f9b7a5bb8ada25f8a3
afika <afikashwashwa@gmail.com>
2024-01-18 21:24:46 +0200

    added my input and button

---
+++ src/components/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const TodoForm = () => {
+  return (
+    <form className='TodoForm'>
+        <input type='text' className='todo-input' placeholder='Add your task!'/>
+        <button type='submit' className='todo-btn'></button>
+
+    </form>
+  )
+}
